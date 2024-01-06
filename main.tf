fa7f6038f835c1839c1101c8914845a3abfd51bf
SRINU <vasu_anand01@yahoo.co.in>
2024-01-06 18:21:25 +0530

    roboshop-parameters

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+#resource "aws_ssm_parameter" "params" {
+#  count = length(var.params)  
+#  name  = var.params[count.index].name
+#  type  = var.params[count.index].type
+#  value = var.params[count.index].value
+#}
+#
+#variable "params" {
+#    default = [
+#        { name = "demo1", value = "demo1", type = "String" },
+#        { name = "demo2", value = "demo2 ", type = "String" }
+#    ]
+#
+#}
+
 resource "aws_ssm_parameter" "params" {
   count = length(var.params)  
   name  = var.params[count.index].name
@@ -7,8 +22,12 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
     default = [
-        { name = "demo1", value = "demo1", type = "String" },
-        { name = "demo2", value = "demo2 ", type = "String" }
+        { name = "roboshop.dev.fronted.catalogue_url", value = "http://catalogue-dev.devopssessions.store", type = "String" },
+        { name = "roboshop.dev.fronted.user_url", value = "http://user-dev.devopssessions.store", type = "String" },
+        { name = "roboshop.dev.fronted.cart_url", value = "http://cart-dev.devopssessions.store", type = "String" },
+        { name = "roboshop.dev.fronted.shipping_url", value = "http://shipping-dev.devopssessions.store ", type = "String" },
+        { name = "roboshop.dev.fronted.payment_url", value = "http://payment-dev.devopssessions.store", type = "String" }
+       
     ]
 
 }
